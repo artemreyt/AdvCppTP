a071a40548279f4ea9dbd9315a39888d275a0299
artem <arstarshov@list.ru>
2020-06-04 21:02:16 +0300

    making HeaderMap

--- hw-6/HttpFramework/include/constants.hpp
+++ hw-6/HttpFramework/include/constants.hpp
@@ -6,17 +6,18 @@ namespace HttpFramework::constants {
     namespace strings {
 
         const char *endline = "\r\n";
-        const char percent = '%';
-        const char question_mark = '?';
+        const char *percent = "%";
+        const char *question_mark = "?";
         const char *whitespaces = " \t";
         const char *headers_end = "\r\n\r\n";
 
         namespace urlencoded {
-            const char pair_separator = '&';
-            const char kv_separator = '=';
+            const char *pair_separator = "&";
+            const char *kv_separator = "=";
         }
 
         namespace headers {
+            const char *separator = ":";
             const char *content_length = "content-length";
 
         }
--- hw-6/HttpFramework/include/http_structures/HeaderMap.hpp
+++ hw-6/HttpFramework/include/http_structures/HeaderMap.hpp
@@ -3,16 +3,30 @@
 
 #include <unordered_map>
 #include <string>
+#include <string_view>
 
 namespace HttpFramework::http_structures {
 
     class HeaderMap {
+        using DataType = std::unordered_map<std::string, std::string>;
+        using const_iterator = DataType::const_iterator;
+
+        explicit HeaderMap(std::string_view headers_view);
+
+        const std::string &get(const std::string &header) const ;
+
+        std::string to_string() const ;
+
+        const_iterator cbegin() const ;
+
+        const_iterator cend() const ;
 
 
 
     private:
 
         std::unordered_map<std::string, std::string> data_;
+        std::string string_image;
     };
 }
 
